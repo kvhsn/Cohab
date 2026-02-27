69724f9467b302be1001fed1af810c811dae80d8
kvhsn <kevinhassan.pro@gmail.com>
2026-02-27 20:23:44 +0100

    fix: eslint configuration

--- packages/eslintconfig.base.js
+++ packages/eslintconfig.base.js
@@ -2,9 +2,11 @@ const { defineConfig } = require('eslint/config');
 const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
+    {
+        ignores: ['dist/**', 'node_modules/**', 'eslint.config.js'],
+    },
     prettierConfig,
     {
-        ignores: ['dist/*', 'node_modules/*', 'eslint.config.js'],
         languageOptions: {
             parserOptions: {
                 tsconfigRootDir: process.cwd(),
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -4,4 +4,8 @@ const expoConfig = require('eslint-config-expo/flat');
 const baseConfig = require('../eslintconfig.base');
 const tanstackQueryConfig = require('@tanstack/eslint-plugin-query');
 
-module.exports = defineConfig([baseConfig, expoConfig, tanstackQueryConfig]);
+module.exports = defineConfig([
+  ...baseConfig,
+  ...expoConfig,
+  ...tanstackQueryConfig.configs['flat/recommended'],
+]);
