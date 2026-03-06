8fc4721ca0cc3f9b47b024c36479a194b524d096
kvhsn <kevinhassan.pro@gmail.com>
2026-03-06 22:17:34 +0100

    feat: Refactor household creation form to utilize custom UI components and enhance the Screen component with a configurable header.

--- packages/ui/app/households/create/index.tsx
+++ packages/ui/app/households/create/index.tsx
@@ -1,20 +1,40 @@
-import { Button, Text, TextInput, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import CustomButton from '@/components/Button/Button';
+import Icon from '@/components/Icon/Icon';
+import Input from '@/components/Input/Input';
+import Screen from '@/components/Screen/Screen';
+import Typography from '@/components/Typography/Typography';
+import { formatErrors } from '@/libs/form';
 import mutations from '@/libs/mutations';
-import { router } from 'expo-router';
-import { createFormHook, createFormHookContexts } from '@tanstack/react-form';
 import { CreateHouseHoldSchema } from '@cohab/shared/src/household';
+import { createFormHook, createFormHookContexts } from '@tanstack/react-form';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Alert, View } from 'react-native';
 
 export default function CreateHousehold() {
+  const router = useRouter();
   const queryClient = useQueryClient();
+
   const { fieldContext, formContext } = createFormHookContexts();
 
   const { useAppForm } = createFormHook({
-    fieldComponents: { TextInput },
-    formComponents: { Button },
+    fieldComponents: { Input },
+    formComponents: { CustomButton },
     fieldContext,
     formContext,
+  });
+
+  const { mutate, isPending } = useMutation({
+    ...mutations.households.createHouseholdMutation(),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['households'] });
+      router.replace('/');
+    },
+    onError: (error: Error) => {
+      console.error(error);
+      Alert.alert('Erreur', 'La création a échoué');
+    },
   });
 
   const form = useAppForm({
@@ -30,41 +50,56 @@ export default function CreateHousehold() {
     },
   });
 
-  const { mutate, isPending } = useMutation({
-    ...mutations.households.createHouseholdMutation(),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['households'] });
-      router.push('/households');
-    },
-  });
-
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Create Household</Text>
-        <View>
-          <form.AppField name="name">
-            {(field) => (
-              <field.TextInput
-                placeholder="Household name"
-                autoCapitalize="none"
-                value={field.state.value}
-                onChangeText={field.handleChange}
-                onBlur={field.handleBlur}
-              />
-            )}
-          </form.AppField>
-          <form.Subscribe selector={(state) => [state.canSubmit]}>
-            {([canSubmit]) => (
-              <form.Button
-                title="Submit"
-                disabled={isPending || !canSubmit}
-                onPress={() => form.handleSubmit()}
-              />
-            )}
-          </form.Subscribe>
+    <View className="flex-1">
+      <Screen headerTitle="Creation de la colocation">
+        <View className="flex-1 py-4">
+          <View className="mt-2 mb-8 gap-3">
+            <Typography variant="h1">Créez votre coloc</Typography>
+            <Typography variant="subtitle">
+              Donnez un nom à votre cocon pour commencer l&apos;aventure.
+            </Typography>
+          </View>
+
+          <View className="gap-2 mb-8">
+            <Typography variant="bodySmall" className="font-bold text-gray-700 ml-1">
+              Nom de la colocation
+            </Typography>
+            <form.AppField name="name">
+              {(field) => (
+                <field.Input
+                  iconName="home-outline"
+                  placeholder="L'Appart du Bonheur"
+                  autoCapitalize="sentences"
+                  value={field.state.value}
+                  onChangeText={field.handleChange}
+                  onBlur={field.handleBlur}
+                  error={
+                    field.state.meta.isTouched ? formatErrors(field.state.meta.errors) : undefined
+                  }
+                />
+              )}
+            </form.AppField>
+          </View>
+
+          <View className="flex-1 justify-end mt-12 pb-8">
+            <form.Subscribe selector={(state) => [state.canSubmit]}>
+              {([canSubmit]) => (
+                <form.CustomButton
+                  variant="primary"
+                  size="lg"
+                  title="Suivant"
+                  disabled={isPending || !canSubmit}
+                  onPress={form.handleSubmit}
+                  RightIcon={({ color: iconColor, size: iconSize }) => (
+                    <Icon as="Ionicons" name="arrow-forward" size={iconSize} color={iconColor} />
+                  )}
+                />
+              )}
+            </form.Subscribe>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </Screen>
+    </View>
   );
 }
--- packages/ui/components/Screen/Screen.tsx
+++ packages/ui/components/Screen/Screen.tsx
@@ -1,3 +1,5 @@
+import { tw } from '@/libs/tailwind';
+import { Stack } from 'expo-router';
 import {
   Keyboard,
   KeyboardAvoidingView,
@@ -10,16 +12,26 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 type ScreenProps = {
   children: React.ReactNode;
   className?: string;
+  headerTitle?: string;
 };
 
-export default function Screen({ children, className }: ScreenProps) {
+export default function Screen({ children, className, headerTitle }: ScreenProps) {
+  const headerShown = !!headerTitle;
+  const paddingTop = headerShown ? tw('mt-6') : '';
   return (
     <KeyboardAvoidingView
       className="flex-1"
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Stack.Screen
+        options={{
+          headerShown,
+          headerTransparent: true,
+          title: headerTitle,
+        }}
+      />
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <ScrollView
-          contentContainerClassName="grow space-between px-6 py-4"
+          contentContainerClassName={`grow space-between px-6 py-4 ${paddingTop}`}
           className={`flex-1 bg-linear-to-br from-background-primary via-background-secondary via-40% to-background-tertiary ${className}`}>
           <SafeAreaView style={{ flex: 1 }}>{children}</SafeAreaView>
         </ScrollView>
