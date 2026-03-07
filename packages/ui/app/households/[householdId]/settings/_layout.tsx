bc25ec1d36796b683066a5b80751b3376f43a845
kvhsn <kevinhassan.pro@gmail.com>
2026-03-07 14:18:30 +0100

    refactor: Consolidate settings layout screens into a single stack and add an iOS-specific back button.

--- packages/ui/app/households/[householdId]/settings/_layout.tsx
+++ packages/ui/app/households/[householdId]/settings/_layout.tsx
@@ -1,24 +1,27 @@
-import { Stack } from 'expo-router';
+import IconButton from '@/components/IconButton/IconButton';
+import { Stack, useRouter } from 'expo-router';
+import { Platform } from 'react-native';
 
 export default function SettingsLayout() {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen
         name="index"
         options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="name"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="members"
-        options={{
-          headerShown: false,
+          title: 'Paramètres',
+          headerLeft:
+            Platform.OS === 'ios'
+              ? () => (
+                  <IconButton
+                    as="Ionicons"
+                    name="chevron-back"
+                    variant="ghost"
+                    onPress={() => router.back()}
+                  />
+                )
+              : undefined,
         }}
       />
     </Stack>
